63a0a2b59410607ff7a2bef04100fc09955bd92d
Jacob Huynh <jacobhuynh314@gmail.com>
2025-05-22 14:07:10 -0400

    feat: add basic css and buttons for host page

--- src/ui/scripts/hostScripts.ts
+++ src/ui/scripts/hostScripts.ts
@@ -2,17 +2,24 @@ import socket from "./socket.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const playerContainer = document.getElementById("player-container");
-  
+  const signOutButton = document.getElementById("host-sign-out-btn");
+
   socket.on("update-host", (names: string[]) => {
     console.log(`My players are ${names}`);
-    if(playerContainer){
+    if (playerContainer) {
       playerContainer.innerHTML = "";
-      names.forEach((name)=>{
+      names.forEach((name) => {
         console.log(name);
-        const newp = document.createElement('p');
+        const newp = document.createElement("p");
         newp.textContent = name;
-        playerContainer.appendChild(newp)
+        playerContainer.appendChild(newp);
       });
     }
   });
+
+  if (signOutButton) {
+    signOutButton.addEventListener("click", () => {
+      socket.emit("leave-game");
+    });
+  }
 });
