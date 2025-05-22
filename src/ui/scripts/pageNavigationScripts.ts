a81791d581134f76a6112528681dead71e6dc15f
Jacob Huynh <jacobhuynh314@gmail.com>
2025-05-22 14:52:43 -0400

    style: clean up unneeded navigation

--- src/ui/scripts/pageNavigationScripts.ts
+++ src/ui/scripts/pageNavigationScripts.ts
@@ -1,16 +1,6 @@
 import socket from "./socket.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const buttons =
-    document.querySelectorAll<HTMLButtonElement>("button[data-page]");
-
-  buttons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const page = btn.getAttribute("data-page");
-      if (page) showPage(page);
-    });
-  });
-
   socket.on("go-to-page", (page: string) => {
     if (page) showPage(page);
     socket.emit("get-page-updates", page);
