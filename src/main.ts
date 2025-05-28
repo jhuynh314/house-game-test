b752a6c0e4859619086d5aae5bf10b5e513ba84d
Jacob Huynh <jacobhuynh314@gmail.com>
2025-05-28 11:48:19 -0400

    feat: run electron app on start

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { spawn } from 'child_process';
+import { app, BrowserWindow, shell } from 'electron';
+
+const createWindow = () =>{
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    win.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(()=>{
+    createWindow();
+});
